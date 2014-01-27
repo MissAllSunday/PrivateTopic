e5548787a12330c2054a58bb87df6b3b2796ba9c
Suki <suki@missallsunday.com>
2014-01-27 17:17:36 -0600

    You know what? lets just use procedural... it really is a pain in the posterior to try and force oop so just use plain old procedural code
    Signed-off-by: Suki <suki@missallsunday.com>

--- PrivateTopics.php
+++ PrivateTopics.php
@@ -25,282 +25,211 @@
  *
  */
 
-function wrapperhandler(){PrivateTopics::handler();}
-
-class PrivateTopics
+function PrivateTopics_doSave($topic, $users)
 {
-	protected $_return;
-	protected $_topic;
-	protected $_users;
-	protected $_request;
-	protected $_board;
-	public static $name = 'PrivateTopics';
+	global $smcFunc;
 
-	public function __construct(){}
+	/* Clean the cache for this topic */
+	cache_put_data(self::$name .':'. $topic, '', 240);
 
-	public function doSave($topic, $users)
+	$smcFunc['db_query']('', '
+		UPDATE {db_prefix}topics
+		SET private_users = {string:users}
+		WHERE id_topic = {int:id}',
+		array(
+			'users' => $this->encode($users),
+		)
+	);
+}
+
+function PrivateTopics_getUsers($topic = 0)
+{
+	global $smcFunc;
+
+	if (empty($topic))
+		return false;
+
+	/* Use the cache when possible */
+	if (($this->_return = cache_get_data(self::$name .':'. $topic, 240)) == null)
 	{
-		global $smcFunc;
-
-		/* Clean the cache for this topic */
-		cache_put_data(self::$name .':'. $topic, '', 240);
-
-		$smcFunc['db_query']('', '
-			UPDATE {db_prefix}topics
-			SET private_users = {string:users}
-			WHERE id_topic = {int:id}',
-			array(
-				'users' => $this->encode($users),
-			)
-		);
-	}
-
-	public function getUsers($topic = 0)
-	{
-		global $smcFunc;
-
-		if (empty($topic))
-			return false;
-
-		/* Use the cache when possible */
-		if (($this->_return = cache_get_data(self::$name .':'. $topic, 240)) == null)
-		{
-			$result = $smcFunc['db_query']('', '
-				SELECT private_users
-				FROM {db_prefix}topics
-				WHERE topic_id = {int:topic}',
-				array(
-					'topic' => $topic,
-				)
-			);
-
-			list ($users) = $smcFunc['db_fetch_row']($result);
-
-			$users = !empty($users) ? $this->decode($users) : array();
-
-			$request = $smcFunc['db_query']('', '
-				SELECT id_member, real_name
-				FROM {db_prefix}members
-				WHERE id_member IN({array_int:users})',
-				array(
-					'users' => $users,
-				)
-			);
-
-			$return = array();
-			while ($row = $smcFunc['db_fetch_assoc']($request))
-				$return[$row['id_member']] = $row['real_name'];
-
-			$smcFunc['db_free_result']($request);
-
-			/* Cache this beauty */
-			cache_put_data(self::$name .':'. $topic, $return, 240);
-		}
-
-		return $return;
-	}
-
-	public function checkBoards($board)
-	{
-		if (empty($board))
-			return false;
-
-		$check = self::setting('boards');
-
-		if (!empty($check))
-			$check = array_values($this->decode($check));
-
-		// No boards means the mod cannot work properly so return false.
-		else
-			return false;
-
-		// Return a boolean, true if the mod is enable on this board.
-		return (isset($check[$board]));s
-	}
-
-	public function doPermissionsSet()
-	{
-		return allowedTo('can_set_topic_as_private');
-	}
-
-	public function doPermissionsSee()
-	{
-		return allowedTo('can_always_see_private_topics');
-	}
-
-	// Don't go around the bush, this mod needs PHP 5.2, anyway, if you need to make a change do it here.
-	public function encode($string)
-	{
-		return json_encode($string);
-	}
-
-	// Always work with arrays
-	public function decode($string, $array = true)
-	{
-		return json_decode($string, $array);
-	}
-
-	public static function text($var)
-	{
-		global $txt;
-
-		if (empty($var))
-			return false;
-
-		// Load the mod's language file.
-		loadLanguage(self::$name);
-
-		if (!empty($txt[self::$name .'_'. $var]))
-			return $txt[self::$name .'_'. $var];
-
-		else
-			return false;
-	}
-
-	public static function enable($var)
-	{
-		global $modSettings;
-
-		if (empty($var))
-			return false;
-
-		if (isset($modSettings[self::$name .'_'. $var]) && !empty($modSettings[self::$name .'_'. $var]))
-			return true;
-
-		else
-			return false;
-	}
-
-	public static function setting($var)
-	{
-		if (empty($var))
-			return false;
-
-		global $modSettings;
-
-		if (true == self::enable($var))
-			return $modSettings[self::$name .'_'. $var];
-
-		else
-			return false;
-	}
-
-	public static function admin(&$admin_areas)
-	{
-		$admin_areas['config']['areas']['privatetopics'] = array(
-			'label' => self::text('title'),
-			'file' => 'PrivateTopics.php',
-			'function' => 'wrapperhandler',
-			'icon' => 'posts.gif',
-			'subsections' => array(
-				'basic' => array(self::text('settings'))
-			),
-		);
-	}
-
-	public static function handler($return_config = false)
-	{
-		global $scripturl, $context, $sourcedir;
-
-		/* I can has Adminz? */
-		isAllowedTo('admin_forum');
-
-		require_once($sourcedir . '/ManageSettings.php');
-
-		$context['page_title'] = self::text('titles');
-
-		$subActions = array(
-			'basic' => 'PrivateTopics::settings'
-		);
-
-		loadGeneralSettingParameters($subActions, 'basic');
-
-		// Load up all the tabs...
-		$context[$context['admin_menu_name']]['tab_data'] = array(
-			'title' => self::text('titles'),
-			'description' => self::text('panel_desc'),
-			'tabs' => array(
-				'basic' => array()
-			),
-		);
-
-		call_user_func($subActions[$_REQUEST['sa']]);
-	}
-
-	public static function settings($return_config = false)
-	{
-		global $txt, $scripturl, $context, $sourcedir, $smcFunc, $modSettings;
-
-		/* I can has Adminz? */
-		isAllowedTo('admin_forum');
-
-		$tools = self::doTools();
-
-		require_once($sourcedir . '/ManageServer.php');
-		loadTemplate('PrivateTopics');
-		loadLanguage('ManageMembers');
-
-		$selected_board = unserialize(self::setting('boards') ? self::setting('boards') : serialize(array()));
-		$context['boards'] = array();
 		$result = $smcFunc['db_query']('', '
-			SELECT id_board, name, child_level
-			FROM {db_prefix}boards
-			ORDER BY board_order',
+			SELECT private_users
+			FROM {db_prefix}topics
+			WHERE topic_id = {int:topic}',
 			array(
+				'topic' => $topic,
 			)
 		);
-		while ($row = $smcFunc['db_fetch_assoc']($result))
-			$context['boards'][$row['id_board']] = array(
-				'id' => $row['id_board'],
-				'name' => $row['name'],
-				'child_level' => $row['child_level'],
-				'selected' => in_array($row['id_board'], $selected_board)
-			);
-		$smcFunc['db_free_result']($result);
 
-		$config_vars = array(
-			array('check', self::$name .'_enable', 'subtext' => self::text('enable_sub')),
-			array('callback', self::$name .'_boards', 'subtext' => self::text('boards_sub')),
-			array('text', self::$name .'_boardindex_message', 'size' => 70, 'subtext' => self::text('boardindex_message_sub')),
+		list ($users) = $smcFunc['db_fetch_row']($result);
 
+		$users = !empty($users) ? $this->decode($users) : array();
+
+		$request = $smcFunc['db_query']('', '
+			SELECT id_member, real_name
+			FROM {db_prefix}members
+			WHERE id_member IN({array_int:users})',
+			array(
+				'users' => $users,
+			)
 		);
 
-		if ($return_config)
-			return $config_vars;
+		$return = array();
+		while ($row = $smcFunc['db_fetch_assoc']($request))
+			$return[$row['id_member']] = $row['real_name'];
 
-		$context['post_url'] = $scripturl . '?action=admin;area=privatetopics;save';
-		$context['settings_title'] = self::text('title');
-		$context['page_title'] = self::text('title');
-		$context['sub_template'] = 'show_settings';
+		$smcFunc['db_free_result']($request);
 
-		if (isset($_GET['save']))
+		/* Cache this beauty */
+		cache_put_data(self::$name .':'. $topic, $return, 240);
+	}
+
+	return $return;
+}
+
+function PrivateTopics_checkBoards($board)
+{
+	if (empty($board))
+		return false;
+
+	$check = self::setting('boards');
+
+	if (!empty($check))
+		$check = array_values($this->decode($check));
+
+	// No boards means the mod cannot work properly so return false.
+	else
+		return false;
+
+	// Return a boolean, true if the mod is enable on this board.
+	return (isset($check[$board]));s
+}
+
+function PrivateTopics_encode($string)
+{
+	return json_encode($string);
+}
+
+function PrivateTopics_decode($string, $array = true)
+{
+	return json_decode($string, $array);
+}
+
+function PrivateTopics_admin(&$admin_areas)
+{
+	$admin_areas['config']['areas']['privatetopics'] = array(
+		'label' => self::text('title'),
+		'file' => 'PrivateTopics.php',
+		'function' => 'wrapperhandler',
+		'icon' => 'posts.gif',
+		'subsections' => array(
+			'basic' => array(self::text('settings'))
+		),
+	);
+}
+
+function PrivateTopics_handler($return_config = false)
+{
+	global $scripturl, $context, $sourcedir;
+
+	/* I can has Adminz? */
+	isAllowedTo('admin_forum');
+
+	require_once($sourcedir . '/ManageSettings.php');
+
+	$context['page_title'] = self::text('titles');
+
+	$subActions = array(
+		'basic' => 'PrivateTopics::settings'
+	);
+
+	loadGeneralSettingParameters($subActions, 'basic');
+
+	// Load up all the tabs...
+	$context[$context['admin_menu_name']]['tab_data'] = array(
+		'title' => self::text('titles'),
+		'description' => self::text('panel_desc'),
+		'tabs' => array(
+			'basic' => array()
+		),
+	);
+
+	call_user_func($subActions[$_REQUEST['sa']]);
+}
+
+function PrivateTopics_settings($return_config = false)
+{
+	global $txt, $scripturl, $context, $sourcedir, $smcFunc, $modSettings;
+
+	/* I can has Adminz? */
+	isAllowedTo('admin_forum');
+
+	$tools = self::doTools();
+
+	require_once($sourcedir . '/ManageServer.php');
+	loadTemplate('PrivateTopics');
+	loadLanguage('ManageMembers');
+
+	$selected_board = unserialize(self::setting('boards') ? self::setting('boards') : serialize(array()));
+	$context['boards'] = array();
+	$result = $smcFunc['db_query']('', '
+		SELECT id_board, name, child_level
+		FROM {db_prefix}boards
+		ORDER BY board_order',
+		array(
+		)
+	);
+	while ($row = $smcFunc['db_fetch_assoc']($result))
+		$context['boards'][$row['id_board']] = array(
+			'id' => $row['id_board'],
+			'name' => $row['name'],
+			'child_level' => $row['child_level'],
+			'selected' => in_array($row['id_board'], $selected_board)
+		);
+	$smcFunc['db_free_result']($result);
+
+	$config_vars = array(
+		array('check', self::$name .'_enable', 'subtext' => self::text('enable_sub')),
+		array('callback', self::$name .'_boards', 'subtext' => self::text('boards_sub')),
+		array('text', self::$name .'_boardindex_message', 'size' => 70, 'subtext' => self::text('boardindex_message_sub')),
+
+	);
+
+	if ($return_config)
+		return $config_vars;
+
+	$context['post_url'] = $scripturl . '?action=admin;area=privatetopics;save';
+	$context['settings_title'] = self::text('title');
+	$context['page_title'] = self::text('title');
+	$context['sub_template'] = 'show_settings';
+
+	if (isset($_GET['save']))
+	{
+		checkSession();
+
+		/* Clean the boards var, we only want integers and nothing else! */
+		if (!empty($_POST['PrivateTopics_boards']))
 		{
-			checkSession();
+			$save_board = array();
 
-			/* Clean the boards var, we only want integers and nothing else! */
-			if (!empty($_POST['PrivateTopics_boards']))
-			{
-				$save_board = array();
+			foreach ($_POST['PrivateTopics_boards'] as $key => $value)
+				if (isset($context['boards'][$value]))
+					$save_board[] = $value;
 
-				foreach ($_POST['PrivateTopics_boards'] as $key => $value)
-					if (isset($context['boards'][$value]))
-						$save_board[] = $value;
-
-				updateSettings(array('PrivateTopics_boards' => serialize($save_board)));
-			}
-
-			saveDBSettings($config_vars);
-			redirectexit('action=admin;area=privatetopics');
+			updateSettings(array('PrivateTopics_boards' => serialize($save_board)));
 		}
 
-		prepareDBSettingContext($config_vars);
+		saveDBSettings($config_vars);
+		redirectexit('action=admin;area=privatetopics');
 	}
 
-	public static function permissions(&$permissionGroups, &$permissionList)
-	{
-		$permissionGroups['membergroup']['simple'] = array('PrivateTopics_per_simple');
-		$permissionGroups['membergroup']['classic'] = array('PrivateTopics_per_classic');
-		$permissionList['membergroup']['can_set_topic_as_private'] = array(false, 'PrivateTopics_per_classic', 'PrivateTopics_per_simple');
-		$permissionList['membergroup']['can_always_see_private_topics'] = array(false, 'PrivateTopics_per_classic', 'PrivateTopics_per_simple');
-	}
+	prepareDBSettingContext($config_vars);
+}
+
+function PrivateTopics_permissions(&$permissionGroups, &$permissionList)
+{
+	$permissionGroups['membergroup']['simple'] = array('PrivateTopics_per_simple');
+	$permissionGroups['membergroup']['classic'] = array('PrivateTopics_per_classic');
+	$permissionList['membergroup']['can_set_topic_as_private'] = array(false, 'PrivateTopics_per_classic', 'PrivateTopics_per_simple');
+	$permissionList['membergroup']['can_always_see_private_topics'] = array(false, 'PrivateTopics_per_classic', 'PrivateTopics_per_simple');
 }
